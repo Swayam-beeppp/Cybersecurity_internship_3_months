af7daae20ac4dbf420053bbee7c04c10fb5d3721
Swayam-beeppp <swayam32109@gmail.com>
2025-07-24 20:54:53 +0530

    Commited

---
+++ Month_1/Assignments/Weekly_Assignments.bat
@@ -0,0 +1,14 @@
+mkdir "Cybersecurity-Internship-Program-2025\Week-1_MITRE-TTP-Mapping\YARA-Rules"
+mkdir "Cybersecurity-Internship-Program-2025\Week-1_MITRE-TTP-Mapping\MISP-Export"
+mkdir "Cybersecurity-Internship-Program-2025\Week-1_MITRE-TTP-Mapping\Screenshots"
+
+mkdir "Cybersecurity-Internship-Program-2025\Week-2_Malware-IOC-APT28\Malware-Reports"
+mkdir "Cybersecurity-Internship-Program-2025\Week-2_Malware-IOC-APT28\IOC-Feeds"
+mkdir "Cybersecurity-Internship-Program-2025\Week-2_Malware-IOC-APT28\MISP-Exports"
+
+mkdir "Cybersecurity-Internship-Program-2025\Week-3_Linux-Privesc\Screenshots"
+mkdir "Cybersecurity-Internship-Program-2025\Week-4_OSINT-Recon"
+mkdir "Cybersecurity-Internship-Program-2025\Week-5_Web-Exploits\SSRF-Test-Scripts"
+mkdir "Cybersecurity-Internship-Program-2025\Week-6_VulnVMs\Screenshots"
+mkdir "Cybersecurity-Internship-Program-2025\Week-7_Log-IR"
+mkdir "Cybersecurity-Internship-Program-2025\Week-8_Finalization\Final-Reports"
